7668688ab2a255e56f77f7c3aeb09abcf329b7d9
unknown <brunoboschi72@gmail.com>
2022-10-21 09:38:34 -0300

    postwordpress 2

--- app/Helpers/ImportadorDadosWordPress.php
+++ app/Helpers/ImportadorDadosWordPress.php
@@ -191,7 +191,7 @@ class ImportadorDadosWordPress
             'Authorization: Basic ' . base64_encode($dadosDominio->usuario_dominio . ':' . $dadosDominio->senha_dominio),
             "cache-control: no-cache",
             'Content-Disposition: attachment; filename=' . $imagem,
-            'Content-Type: image/jpg'
+            'Content-Type: image/webp'
         );
 
         curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
